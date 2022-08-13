af26e36e8006c482c2d0b6d180717b409b498770
Bryan Ju <jubryanju@gmail.com>
2022-08-12 19:56:23 -0500

    init

---
+++ Assets/New Folder/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        
+        if(Mouse.current.rightButton)
+
+    }
+}
