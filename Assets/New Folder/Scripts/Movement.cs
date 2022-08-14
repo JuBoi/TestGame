f630056ce6db5045c7e3b14da11e9021c769f7a4
Bryan Ju <jubryanju@gmail.com>
2022-08-13 20:03:44 -0500

    stuff

--- Assets/New Folder/Scripts/Movement.cs
+++ Assets/New Folder/Scripts/Movement.cs
@@ -2,22 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class Movement : MonoBehaviour {
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private InputAction mouseClickAction;
+
+    [SerializeField]
+    private float playerSpeed = 10;
+
+    private Camera mainCamera;
+    private Coroutine coroutine;
+    private Vector3 targetPosition;
+    private Rigidbody rb;
+
+    public Tilemap map;
+
+    private Vector3 destination;
+
+    private void Awake()
     {
-        
+        mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        mouseClickAction.Enable();
+        mouseClickAction.performed += Move;
+    }
+
+    private void OnDisable()
+    {
+        mouseClickAction.performed -= Move;
+        mouseClickAction.Disable();
+    }
+
+    private void Move(InputAction.CallbackContext context)
     {
 
-        Vector2 mousePosition = Mouse.current.position.ReadValue();
-        
-        if(Mouse.current.rightButton)
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
+        if(Physics.Raycast(ray: ray, hitInfo: out RaycastHit hit) && hit.collider && hit.collider.CompareTag("Ground"))
+        {
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+            }
+            coroutine = StartCoroutine(PlayerMoveTowards(hit.point));
+            targetPosition = hit.point;
+        }
+    }
+
+    private IEnumerator PlayerMoveTowards(Vector3 target)
+    {
+        while(Vector3.Distance(transform.position, target) > 0.1f)
+        {
+            Vector3 destination = Vector3.MoveTowards(rb.position, target, playerSpeed * Time.deltaTime);
+            rb.position = destination;
+            yield return null;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(!collision.gameObject.CompareTag("Ground"))
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(targetPosition, 1);
     }
 }
